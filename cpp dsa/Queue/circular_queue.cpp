60679f8f132905c30c69318246beadf7c9ded66f
ashutosh786kr <ashuronnie1234@gmail.com>
2024-05-27 06:14:42 +0530

    some further changes in the impleentation of circular queue

--- cpp dsa/Queue/circular_queue.cpp
+++ cpp dsa/Queue/circular_queue.cpp
@@ -18,7 +18,7 @@ class CircularQueue{
 
     // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
     bool enqueue(int value){
-        //to check whther queue is full
+        //to check wether queue is full
         if( (front == 0 && rear == size-1) || (rear == (front-1)%(size-1) ) ) {
             //cout << "Queue is Full";
             return false;
@@ -26,18 +26,20 @@ class CircularQueue{
         else if(front == -1) //first element to push
         {
 			front = rear = 0;
+            arr[rear] = value;
             
         }
+        //it means that front is somewhere in the midddle and rear is at the last and hence we gotto push after we move rear 
         else if(rear == size-1 && front != 0) {
-            rear = 0; //to maintain cyclic nature
+            rear = 0; //to maintain cyclic nature rear was at size-1 and hece it is moved to first porsition
+            arr[rear] = value;
         }
         else
-        {//normal flow
+        {
+            //normal flow
             rear++;
+            arr[rear] = value;
         }
-        //push inside the queue
-        arr[rear] = value;
-        
         return true;
     }
 
@@ -47,18 +49,25 @@ class CircularQueue{
             //cout << "Queue is Empty " << endl;
             return -1;
         }
+
+        //lets first take the value of front index and to deque we will have to fill with -1 adn then handle the front according to conditions
         int ans = arr[front];
         arr[front] = -1;
+
         if(front == rear) { //single element is present
             front = rear = -1;
         }
+        //front is sitting at the last end and hence to make it cyclic ,front is moved to 0th index
         else if(front == size - 1) {
             front = 0; //to maintain cyclic nature
         }
+
         else
-        {//normal flow
+        {
+            //normal flow
             front++;
         }
+
         return ans;
     }
 };
